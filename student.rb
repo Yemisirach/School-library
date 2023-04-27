696904f63deb98667b130efec8b644ac824c8e69
Yemisirach <yemsrach3723@gmail.com>
2023-04-27 13:00:18 +0300

    set up association

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require './person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom, age, name: 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -8,6 +10,12 @@ class Student < Person
 
   def play_hooky
     '"¯\(ツ)/¯"'
+  end
+
+  # setter
+  def update_classroom(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
 
