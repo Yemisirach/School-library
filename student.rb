2db4040aaeb101009d4799e5a24852b98b95f7e0
Yemisirach <yemsrach3723@gmail.com>
2023-04-29 20:51:32 +0300

    Fix linter error

--- student.rb
+++ student.rb
@@ -4,7 +4,7 @@ require './classroom'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(classroom, age, name = 'Unknown', parent_permission = true)
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
